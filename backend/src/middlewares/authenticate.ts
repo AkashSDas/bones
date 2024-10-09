8a20fce1a7cb87e8457faf18c7ada02e4f3ef2e6
AkashSDas <aakashdas368@gmail.com>
2024-10-09 23:56:14 +0530

    [be] refactor middlewares

--- backend/src/middlewares/authenticate.ts
+++ backend/src/middlewares/authenticate.ts
@@ -3,11 +3,12 @@ import { env } from "@/utils/env";
 import { createMiddleware } from "hono/factory";
 import jwt from "jsonwebtoken";
 
+import { log } from "@/lib/logger";
 import { auth } from "@/utils/auth";
 import { UnauthorizedError } from "@/utils/http";
 import type { AppBindings } from "@/utils/types";
 
-export const authenticateMiddleware = createMiddleware<AppBindings>(async (c, next) => {
+export const authenticate = createMiddleware<AppBindings>(async (c, next) => {
     const authHeader = c.req.header("Authorization");
 
     if (authHeader === undefined) {
@@ -35,14 +36,21 @@ export const authenticateMiddleware = createMiddleware<AppBindings>(async (c, ne
 
     try {
         const decoded = jwt.verify(accessToken, env.ACCESS_TOKEN_SECRET);
-        const payload = await auth.getAccessTokenContent(decoded);
+        const payload = await auth.extractJWTContent(decoded);
 
         if (payload === null) {
             throw new Error("Payload content is missing");
         } else {
-            c.set("jwtContent", payload);
+            if (payload["type"] === "account") {
+                c.set("accountJWTContent", payload);
+            } else if (payload["type"] === "user") {
+                c.set("userJWTContent", payload);
+            } else {
+                throw new Error(`Invalid payload type: ${payload["type"]}`);
+            }
         }
     } catch (e) {
+        log.error(`Failed to verify access token: ${e}`);
         throw new UnauthorizedError({
             message: "Unauthorized",
             reason: "Invalid or expired access token",
--- backend/src/middlewares/correlation-id.ts
+++ backend/src/middlewares/correlation-id.ts
@@ -2,7 +2,7 @@ import type { Context, Next } from "hono";
 import { v7 as uuid } from "uuid";
 
 /** Middleware for setting up correlation ID */
-export async function correlationIdMiddleware(c: Context, next: Next) {
+export async function correlationId(c: Context, next: Next): Promise<void> {
     const correlationId = c.req.header("X-Correlation-ID") ?? uuid();
     c.header("X-Correlation-ID", correlationId);
     c.set("correlationId", correlationId);
