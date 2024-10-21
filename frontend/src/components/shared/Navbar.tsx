71d545ffb6fe2b6604d9971db129474d01008792
AkashSDas <aakashdas368@gmail.com>
2024-10-21 21:55:44 +0530

    [fe] add links in navbar

--- frontend/src/components/shared/Navbar.tsx
+++ frontend/src/components/shared/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Link } from "@tanstack/react-router";
 import { MenuIcon, SearchIcon } from "lucide-react";
 import React from "react";
 
@@ -8,18 +9,27 @@ import { Button } from "./Button";
 export function Navbar(): React.JSX.Element {
     return (
         <nav className="flex items-center justify-between w-full h-12 gap-2 px-4 border-b md:h-14 md:px-8 bg-grey-950 border-b-grey-800">
-            <Logo className="h-[23.15px] w-[37.98px] md:h-[30.86px] md:w-[50.64px]" />
+            <Link to="/">
+                <Logo className="h-[23.15px] w-[37.98px] md:h-[30.86px] md:w-[50.64px]" />
+            </Link>
 
             <Button variant="secondary" size="icon" className="visible md:hidden">
                 <MenuIcon />
             </Button>
 
             <div className="items-center justify-end hidden gap-4 md:flex">
+                {/* TODO: this should open a search so logged in and not logged in user can search/navigate */}
                 <Button size="icon" variant="ghost">
                     <SearchIcon />
                 </Button>
-                <Button variant="secondary">Login</Button>
-                <Button>Create Account</Button>
+
+                <Button variant="secondary" asChild>
+                    <Link to="/auth/login">Login</Link>
+                </Button>
+
+                <Button>
+                    <Link to="/auth/signup">Create Account</Link>
+                </Button>
             </div>
         </nav>
     );
