d08f7247bd99247e8f5c77d0f2c856be3c2de3ca
AkashSDas <aakashdas368@gmail.com>
2024-10-24 08:45:32 +0530

    [fe] add markup for forgot password and reset password

--- frontend/src/routes/auth/forgot-password.tsx
+++ frontend/src/routes/auth/forgot-password.tsx
@@ -1,5 +1,79 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { createFileRoute } from "@tanstack/react-router";
+import type React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/shared/Button";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/shared/Form";
+import { Input } from "@/components/shared/Input";
 
 export const Route = createFileRoute("/auth/forgot-password")({
-    component: () => <div>Hello /auth/forgot-password!</div>,
+    component: ForgotPasswordPage,
 });
+
+const FormSchema = z.object({
+    email: z.string().email(),
+});
+
+function ForgotPasswordPage(): React.JSX.Element {
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: { email: "" },
+    });
+
+    function onSubmit(values: z.infer<typeof FormSchema>) {
+        console.log({ values });
+    }
+
+    return (
+        <main className="my-2 md:my-6 px-4 md:py-8 mx-auto w-full max-w-[680px] space-y-4 md:space-y-4">
+            <h1 className="h2">Forgot Password</h1>
+
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-6 md:space-y-8"
+                >
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Email Address</FormLabel>
+                                <FormDescription>
+                                    Password reset instructions will be sent to your
+                                    registered Bones account
+                                </FormDescription>
+                                <FormControl>
+                                    <Input
+                                        placeholder="example@gmail.com"
+                                        type="email"
+                                        required
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <Button
+                        type="submit"
+                        className="w-full h-[38px] md:h-[46px] text-sm md:text-base"
+                    >
+                        Send Instructions
+                    </Button>
+                </form>
+            </Form>
+        </main>
+    );
+}
--- frontend/src/routes/auth/reset-password.tsx
+++ frontend/src/routes/auth/reset-password.tsx
@@ -1,5 +1,90 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { createFileRoute } from "@tanstack/react-router";
+import type React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/shared/Button";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/shared/Form";
+import { Input } from "@/components/shared/Input";
+import { PasswordStrengthBar } from "@/components/shared/PasswordStrengthBar";
+import { usePasswordStrength } from "@/hooks/form";
 
 export const Route = createFileRoute("/auth/reset-password")({
-    component: () => <div>Hello /auth/reset-password!</div>,
+    component: ResetPasswordPage,
 });
+
+const FormSchema = z.object({
+    newPassword: z.string().email(),
+});
+
+function ResetPasswordPage(): React.JSX.Element {
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: { newPassword: "" },
+    });
+
+    const strength = usePasswordStrength(
+        () => form.getValues().newPassword,
+        form.watch("newPassword"),
+    );
+
+    function onSubmit(values: z.infer<typeof FormSchema>) {
+        console.log({ values });
+    }
+
+    return (
+        <main className="my-2 md:my-6 px-4 md:py-8 mx-auto w-full max-w-[680px] space-y-4 md:space-y-4">
+            <h1 className="h2"></h1>
+
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-6 md:space-y-8"
+                >
+                    <FormField
+                        control={form.control}
+                        name="newPassword"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormDescription>
+                                    Add your new account for your Bones account
+                                </FormDescription>
+                                <FormControl>
+                                    <Input
+                                        type="password"
+                                        minLength={8}
+                                        maxLength={255}
+                                        required
+                                        {...field}
+                                    />
+                                </FormControl>
+
+                                <PasswordStrengthBar
+                                    strengthPercentage={strength.score}
+                                />
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <Button
+                        type="submit"
+                        className="w-full h-[38px] md:h-[46px] text-sm md:text-base"
+                    >
+                        Change Password
+                    </Button>
+                </form>
+            </Form>
+        </main>
+    );
+}
