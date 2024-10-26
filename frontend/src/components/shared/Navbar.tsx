d8b84de79eef530b7b64e3e327636c84384329e4
AkashSDas <aakashdas368@gmail.com>
2024-10-26 21:09:29 +0530

    [fe] make navbar dynamic

--- frontend/src/components/shared/Navbar.tsx
+++ frontend/src/components/shared/Navbar.tsx
@@ -7,6 +7,7 @@ import { useAuth, useLogout } from "@/hooks/auth";
 
 import { Button } from "./Button";
 import { Loader } from "./Loader";
+import { Sheet, SheetClose, SheetContent, SheetFooter, SheetTrigger } from "./Sheet";
 import { Skeleton } from "./Skeleton";
 
 export function Navbar(): React.JSX.Element {
@@ -19,9 +20,7 @@ export function Navbar(): React.JSX.Element {
                 <Logo className="h-[23.15px] w-[37.98px] md:h-[30.86px] md:w-[50.64px]" />
             </Link>
 
-            <Button variant="secondary" size="icon" className="visible md:hidden">
-                <MenuIcon />
-            </Button>
+            <MobileSideMenu />
 
             <div className="items-center justify-end hidden gap-4 md:flex">
                 {/* TODO: this should open a search so logged in and not logged in user can search/navigate */}
@@ -49,5 +48,53 @@ export function Navbar(): React.JSX.Element {
                 )}
             </div>
         </nav>
+    );
+}
+
+function MobileSideMenu(): React.JSX.Element {
+    const { isLoading, isLoggedIn } = useAuth();
+    const { isPending, logout } = useLogout();
+
+    return (
+        <Sheet>
+            <SheetTrigger asChild>
+                <Button variant="secondary" size="icon" className="visible md:hidden">
+                    <MenuIcon />
+                </Button>
+            </SheetTrigger>
+
+            <SheetContent>
+                <SheetFooter>
+                    {isLoading ? (
+                        <Skeleton className="w-full h-9 min-w-32" />
+                    ) : isLoggedIn ? (
+                        <SheetClose>
+                            <Button
+                                variant="secondary"
+                                onClick={logout}
+                                disabled={isPending}
+                            >
+                                {isPending ? <Loader sizeInPx={18} /> : null}
+                                {isPending ? "Logging Out" : "Logout"}
+                            </Button>
+                        </SheetClose>
+                    ) : (
+                        <div className="flex flex-col w-full gap-2">
+                            <SheetClose asChild>
+                                <Button variant="secondary" asChild className="w-full">
+                                    <Link to="/auth/login">Login</Link>
+                                </Button>
+                            </SheetClose>
+
+                            <SheetClose asChild>
+                                <Button asChild className="w-full">
+                                    <Link to="/auth/signup">Create Account</Link>
+                                </Button>
+                            </SheetClose>
+                        </div>
+                    )}
+                </SheetFooter>
+            </SheetContent>
+        </Sheet>
     );
 }
