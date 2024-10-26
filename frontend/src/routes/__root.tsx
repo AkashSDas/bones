4d58329221533e7a8c63a4cf98cad3115616dafc
AkashSDas <aakashdas368@gmail.com>
2024-10-26 13:40:24 +0530

    [fe] setup react query

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,14 +1,32 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { useState } from "react";
 
 import { Navbar } from "@/components/shared/Navbar";
 
 export const Route = createRootRoute({
-    component: () => (
-        <>
+    component: Root,
+});
+
+function Root(): React.JSX.Element {
+    const [queryClient] = useState(() => {
+        return new QueryClient({
+            defaultOptions: {
+                queries: {
+                    refetchOnWindowFocus: false,
+                    refetchOnMount: false,
+                    retry: false,
+                },
+            },
+        });
+    });
+
+    return (
+        <QueryClientProvider client={queryClient}>
             <Navbar />
             <Outlet />
             <TanStackRouterDevtools />
-        </>
-    ),
-});
+        </QueryClientProvider>
+    );
+}
