bae74ba439209143775a2156af6aee3d4d090581
AkashSDas <aakashdas368@gmail.com>
2024-10-26 14:14:57 +0530

    [fe] add reset password integration

--- frontend/src/routes/auth/reset-password.tsx
+++ frontend/src/routes/auth/reset-password.tsx
@@ -1,6 +1,8 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { fallback, zodSearchValidator } from "@tanstack/router-zod-adapter";
 import type React from "react";
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -15,18 +17,28 @@ import {
     FormMessage,
 } from "@/components/shared/Form";
 import { Input } from "@/components/shared/Input";
+import { Loader } from "@/components/shared/Loader";
 import { PasswordStrengthBar } from "@/components/shared/PasswordStrengthBar";
+import { ToastAction } from "@/components/shared/toast";
+import { usePostApiV1IamAccountResetPasswordResetToken } from "@/gen/endpoints/iam-account/iam-account";
 import { usePasswordStrength } from "@/hooks/form";
+import { useToast } from "@/hooks/toast";
+
+const FormSchema = z.object({ newPassword: z.string() });
+const SearchSchema = z.object({
+    token: fallback(z.string().length(16), "").default(""),
+});
 
 export const Route = createFileRoute("/auth/reset-password")({
     component: ResetPasswordPage,
-});
-
-const FormSchema = z.object({
-    newPassword: z.string().email(),
+    validateSearch: zodSearchValidator(SearchSchema),
 });
 
 function ResetPasswordPage(): React.JSX.Element {
+    const { token } = Route.useSearch();
+    const navigate = useNavigate({ from: "/auth/reset-password" });
+    const { toast } = useToast();
+
     const form = useForm<z.infer<typeof FormSchema>>({
         resolver: zodResolver(FormSchema),
         defaultValues: { newPassword: "" },
@@ -37,13 +49,58 @@ function ResetPasswordPage(): React.JSX.Element {
         form.watch("newPassword"),
     );
 
-    function onSubmit(values: z.infer<typeof FormSchema>) {
-        console.log({ values });
+    const mutation = usePostApiV1IamAccountResetPasswordResetToken({
+        mutation: {
+            onError(e) {
+                toast({
+                    variant: "error",
+                    title: "Failed",
+                    description: e.response?.data.message,
+                    action: (
+                        <ToastAction
+                            altText="Try again"
+                            onClick={() => navigate({ to: "/auth/forgot-password" })}
+                        >
+                            Try Again
+                        </ToastAction>
+                    ),
+                });
+            },
+            onSuccess(data) {
+                toast({
+                    variant: "success",
+                    title: "Password Updated",
+                    description: data.data.message,
+                });
+                navigate({ to: "/auth/login" });
+            },
+        },
+    });
+
+    async function onSubmit(values: z.infer<typeof FormSchema>) {
+        await mutation.mutateAsync({
+            data: { password: values.newPassword },
+            resetToken: token,
+        });
     }
+
+    useEffect(
+        function redirectOnEmptyToken() {
+            if (!token) {
+                toast({
+                    variant: "error",
+                    title: "Invalid or expired link",
+                    description: "Invalid password reset link. Try again",
+                });
+                navigate({ to: "/auth/forgot-password" });
+            }
+        },
+        [token],
+    );
 
     return (
         <main className="my-5 md:my-6 px-4 md:py-8 mx-auto w-full max-w-[680px] space-y-4 md:space-y-4">
-            <h1 className="h2"></h1>
+            <h1 className="h2">Reset Password</h1>
 
             <Form {...form}>
                 <form
@@ -80,8 +137,9 @@ function ResetPasswordPage(): React.JSX.Element {
                     <Button
                         type="submit"
                         className="w-full h-[38px] md:h-[46px] text-sm md:text-base"
+                        disabled={mutation.isPending}
                     >
-                        Change Password
+                        {mutation.isPending ? <Loader /> : "Change Password"}
                     </Button>
                 </form>
             </Form>
