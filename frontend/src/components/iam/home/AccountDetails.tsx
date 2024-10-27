be573c5a8de3b9a3f8b949ddd360877c4de9c9ec
AkashSDas <aakashdas368@gmail.com>
2024-10-27 18:08:17 +0530

    [fe] create components for IAM home page

---
+++ frontend/src/components/iam/home/AccountDetails.tsx
@@ -0,0 +1,78 @@
+import { Link } from "@tanstack/react-router";
+
+import { Button } from "@/components/shared/Button";
+import { useAuth } from "@/hooks/auth";
+import { useToast } from "@/hooks/toast";
+import { formatDate } from "@/utils/datetime";
+
+import { AdminOnlyCard } from "./AdminOnlyCard";
+import { Card } from "./Card";
+
+export function AccountDetails() {
+    const { account } = useAuth();
+    const { toast } = useToast();
+
+    if (account === undefined) {
+        return null;
+    }
+
+    return (
+        <section className="w-full max-w-[740px] mx-auto">
+            <h2 className="mb-4 h3">Account</h2>
+
+            <div className="p-4 border rounded-card border-grey-800 border-card flex flex-col gap-[14px] divide-y divide-grey-800">
+                <Card
+                    title="Account ID"
+                    useSelectAllForDescription
+                    description={account.accountId}
+                    action={
+                        <span className="text-sm text-grey-500">
+                            Created on {formatDate(account.createdAt, "DD MMM YYYY")}
+                        </span>
+                    }
+                />
+
+                <Card
+                    title="Account Name"
+                    useSelectAllForDescription
+                    description={account.accountName}
+                />
+
+                <AdminOnlyCard
+                    title="Password"
+                    description={`Last changed on ${formatDate(account.passwordAge, "DD MMM YYYY")}`}
+                    action={
+                        <Link to="/auth/forgot-password" target="_blank">
+                            <Button variant="secondary">Change</Button>
+                        </Link>
+                    }
+                />
+
+                <AdminOnlyCard
+                    title="Verification"
+                    description={
+                        account.lastLoggedInAt
+                            ? `Last verified on ${formatDate(account.lastVerifiedAt!, "DD MMM YYYY")}`
+                            : `Not verified yet`
+                    }
+                    action={
+                        account.lastLoggedInAt ? null : (
+                            <Button
+                                variant="secondary"
+                                onClick={() => {
+                                    toast({
+                                        variant: "info",
+                                        title: "Support",
+                                        description: `Connect with BONES support team`,
+                                    });
+                                }}
+                            >
+                                Verify
+                            </Button>
+                        )
+                    }
+                />
+            </div>
+        </section>
+    );
+}
---
+++ frontend/src/components/iam/home/IAMUsersDetails.tsx
@@ -0,0 +1,60 @@
+import { Link } from "@tanstack/react-router";
+
+import { Button } from "@/components/shared/Button";
+import { Loader } from "@/components/shared/Loader";
+import { useGetApiV1IamUser } from "@/gen/endpoints/iam-user/iam-user";
+import { useAuthStore } from "@/hooks/auth";
+
+import { Card } from "./Card";
+
+export function IAMUsersDetails() {
+    const authHeader = useAuthStore((s) => s.bearerTokenHeader);
+    const query = useGetApiV1IamUser(
+        { limit: "1" },
+        {
+            axios: { headers: { ...authHeader() } },
+            query: {
+                initialData: undefined,
+                queryKey: ["asd"],
+            },
+        },
+    );
+
+    const dataLoaded = query.data !== undefined;
+
+    return (
+        <section className="w-full max-w-[740px] mx-auto">
+            <h2 className="mb-4 h3">IAM User</h2>
+
+            <div className="p-4 transition-height border rounded-card border-grey-800 border-card flex flex-col gap-[14px] divide-y divide-grey-800">
+                {query.isLoading || query.isFetching ? (
+                    <Loader variant="section" />
+                ) : null}
+
+                {dataLoaded ? (
+                    <>
+                        <Card
+                            title="Total IAM Users"
+                            description={`There are ${query.data.data.total} IAM users`}
+                            action={
+                                <Link to="/iam/users">
+                                    <Button variant="secondary">See all</Button>
+                                </Link>
+                            }
+                        />
+
+                        <Card
+                            title="New IAM User"
+                            description={`Create a new user for this account`}
+                            action={
+                                <Link to="/iam/users/new">
+                                    <Button variant="secondary">Add</Button>
+                                </Link>
+                            }
+                        />
+                    </>
+                ) : null}
+            </div>
+        </section>
+    );
+}
