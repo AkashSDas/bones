045380f6bd3ea935762c42f392fea71730d10780
AkashSDas <aakashdas368@gmail.com>
2024-10-29 23:10:07 +0530

    [fe] refactor IAM users table

--- frontend/src/routes/iam/users/index.tsx
+++ frontend/src/routes/iam/users/index.tsx
@@ -1,5 +1,5 @@
 import { keepPreviousData } from "@tanstack/react-query";
-import { createFileRoute } from "@tanstack/react-router";
+import { Link, createFileRoute } from "@tanstack/react-router";
 import {
     createColumnHelper,
     flexRender,
@@ -7,10 +7,20 @@ import {
     useReactTable,
 } from "@tanstack/react-table";
 import { fallback, zodSearchValidator } from "@tanstack/router-zod-adapter";
+import {
+    BanIcon,
+    CalendarIcon,
+    ClockIcon,
+    ExternalLinkIcon,
+    IdCardIcon,
+    LockIcon,
+    SignatureIcon,
+} from "lucide-react";
 import { useMemo } from "react";
 import { z } from "zod";
 
 import { AuthProtected } from "@/components/shared/AuthProtected";
+import { Button } from "@/components/shared/Button";
 import { Loader } from "@/components/shared/Loader";
 import { useGetApiV1IamUser } from "@/gen/endpoints/iam-user/iam-user";
 import { useAuth } from "@/hooks/auth";
@@ -46,29 +56,72 @@ const columnHelper = createColumnHelper<IAMUserRow>();
 
 const columns = [
     columnHelper.accessor("userId", {
-        header: () => "User ID",
-        cell: (info) => <span className="select-all">{info.getValue()}</span>,
+        header: () => (
+            <span className="flex items-center gap-2">
+                <IdCardIcon size="16px" />
+                User ID
+            </span>
+        ),
+        cell: (info) => {
+            const userId = info.cell.row.original.userId;
+
+            return (
+                <div className="relative min-w-full group">
+                    <span className="select-all">{info.getValue()}</span>
+
+                    <Link to="/iam/users/$userId" params={{ userId }} target="_blank">
+                        <Button
+                            variant="secondary"
+                            className="absolute right-0 !px-2 text-xs font-normal tracking-wide -translate-y-1/2 shadow-lg md:hidden md:group-hover:flex max-h-7 text-grey-500 top-1/2"
+                        >
+                            <ExternalLinkIcon />
+                            OPEN
+                        </Button>
+                    </Link>
+                </div>
+            );
+        },
         minSize: 200,
         maxSize: 200,
     }),
     columnHelper.accessor("username", {
-        header: () => "Username",
+        header: () => (
+            <span className="flex items-center gap-2">
+                <SignatureIcon size="16px" />
+                Username
+            </span>
+        ),
         cell: (info) => <span className="select-all">{info.getValue()}</span>,
     }),
     columnHelper.accessor("passwordAge", {
-        header: () => "Password Age",
+        header: () => (
+            <span className="flex items-center gap-2">
+                <LockIcon size="16px" />
+                Username
+            </span>
+        ),
         cell: (info) => timeAgo(info.getValue()),
     }),
     columnHelper.accessor("lastLoggedInAt", {
-        header: () => "Last Login",
-        cell: (info) => formatDate(info.getValue(), "DD MMM YYYY AM/PM"),
+        header: () => (
+            <span className="flex items-center gap-2">
+                <ClockIcon size="16px" />
+                Last Login
+            </span>
+        ),
+        cell: (info) => timeAgo(info.getValue()),
     }),
     columnHelper.accessor("isBlocked", {
-        header: () => "Blocked",
+        header: () => (
+            <span className="flex items-center gap-2">
+                <BanIcon size="16px" />
+                Blocked
+            </span>
+        ),
         cell: (info) => (
             <span
                 className={cn(
-                    "font-medium tracking-wider uppercase rounded-sm px-2 py-1 text-white",
+                    "font-medium text-xs !h-6 tracking-wider uppercase rounded-sm px-4 py-1 text-white",
                     info.getValue() ? "bg-error-500" : "bg-success-500",
                 )}
             >
@@ -78,7 +131,12 @@ const columns = [
         maxSize: 34,
     }),
     columnHelper.accessor("createdOn", {
-        header: () => "Created On",
+        header: () => (
+            <span className="flex items-center gap-2">
+                <CalendarIcon size="16px" />
+                Created On
+            </span>
+        ),
         cell: (info) => formatDate(info.getValue(), "DD MMM YYYY AM/PM"),
     }),
 ];
@@ -138,73 +196,75 @@ function IAMUsersView() {
 
             {query.isLoading ? <Loader variant="page" /> : null}
 
-            <table className="min-w-full p-1 overflow-x-auto border w-fit rounded-card border-grey-800 border-card">
-                <thead className="border-b border-b-grey-800">
-                    {table.getHeaderGroups().map(function (headerGroup) {
-                        return (
-                            <tr
-                                key={headerGroup.id}
-                                className="divide-x text-start divide-grey-800 h-9"
-                            >
-                                {headerGroup.headers.map(function (header) {
-                                    return (
-                                        <th
-                                            key={header.id}
-                                            className={cn(
-                                                "relative px-2 text-sm font-medium text-start group",
-                                            )}
-                                            style={{
-                                                width: `${header.getSize()}px`,
-                                            }}
-                                        >
-                                            {flexRender(
-                                                header.column.columnDef.header,
-                                                header.getContext(),
-                                            )}
-
-                                            <div
-                                                onMouseDown={header.getResizeHandler()}
-                                                onTouchStart={header.getResizeHandler()}
+            <section className="overflow-x-auto">
+                <table className="p-1 table-auto w-full !mt-0 border rounded-card border-grey-800">
+                    <thead className="border-b border-b-grey-800">
+                        {table.getHeaderGroups().map(function (headerGroup) {
+                            return (
+                                <tr
+                                    key={headerGroup.id}
+                                    className="divide-x text-start divide-grey-800 h-9"
+                                >
+                                    {headerGroup.headers.map(function (header) {
+                                        return (
+                                            <th
+                                                key={header.id}
                                                 className={cn(
-                                                    "absolute cursor-ew-resize -right-[2px] opacity-0 group-hover:opacity-100 transition-opacity top-0 w-[3px] h-[39px] bg-info-500",
-                                                    header.column.getIsResizing()
-                                                        ? "bg-info-600"
-                                                        : null,
+                                                    "text-nowrap relative px-2 text-sm font-medium text-grey-500 text-start group",
                                                 )}
-                                            />
-                                        </th>
-                                    );
-                                })}
-                            </tr>
-                        );
-                    })}
-                </thead>
+                                                style={{
+                                                    width: `${header.getSize()}px`,
+                                                }}
+                                            >
+                                                {flexRender(
+                                                    header.column.columnDef.header,
+                                                    header.getContext(),
+                                                )}
 
-                <tbody className="divide-y divide-grey-800">
-                    {table.getRowModel().rows.map(function (row) {
-                        return (
-                            <tr
-                                key={row.id}
-                                className="divide-x text-start divide-grey-800 h-9"
-                            >
-                                {row.getVisibleCells().map(function (cell) {
-                                    return (
-                                        <td
-                                            key={cell.id}
-                                            className="px-2 text-sm font-normal text-start text-grey-400"
-                                        >
-                                            {flexRender(
-                                                cell.column.columnDef.cell,
-                                                cell.getContext(),
-                                            )}
-                                        </td>
-                                    );
-                                })}
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
+                                                <div
+                                                    onMouseDown={header.getResizeHandler()}
+                                                    onTouchStart={header.getResizeHandler()}
+                                                    className={cn(
+                                                        "absolute cursor-col-resize -right-[3px] opacity-0 group-hover:opacity-100 hover:opacity-100 active:opacity-100 transition-opacity top-0 w-[6px] h-[36px] bg-info-400",
+                                                        header.column.getIsResizing()
+                                                            ? "bg-info-500"
+                                                            : null,
+                                                    )}
+                                                />
+                                            </th>
+                                        );
+                                    })}
+                                </tr>
+                            );
+                        })}
+                    </thead>
+
+                    <tbody className="divide-y divide-grey-800">
+                        {table.getRowModel().rows.map(function (row) {
+                            return (
+                                <tr
+                                    key={row.id}
+                                    className="divide-x text-start divide-grey-800 h-9"
+                                >
+                                    {row.getVisibleCells().map(function (cell) {
+                                        return (
+                                            <td
+                                                key={cell.id}
+                                                className="px-2 text-sm font-normal text-nowrap text-start text-grey-100"
+                                            >
+                                                {flexRender(
+                                                    cell.column.columnDef.cell,
+                                                    cell.getContext(),
+                                                )}
+                                            </td>
+                                        );
+                                    })}
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </section>
         </main>
     );
 }
