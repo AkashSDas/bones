c9f8df81d08c5a53a062a5eb2c2252544ff47f04
AkashSDas <aakashdas368@gmail.com>
2024-11-11 22:36:45 +0530

    [bridge] update start script and setup web socket

--- bridge/src/index.ts
+++ bridge/src/index.ts
@@ -1,4 +1,5 @@
 import { type Context, Hono } from "jsr:@hono/hono";
+import { upgradeWebSocket } from "jsr:@hono/hono/deno";
 import { status } from "#utils/http.ts";
 
 const app = new Hono();
@@ -6,5 +7,23 @@ const app = new Hono();
 app.get("/ping", (c: Context) => {
     return c.json({ message: "Bridge is active" }, status.OK);
 });
+
+app.get(
+    "/ws",
+    upgradeWebSocket((_c) => {
+        return {
+            onOpen: () => {
+                console.log("Connection opened");
+            },
+            onMessage(event, ws) {
+                console.log(`Message from client: ${event.data}`);
+                ws.send("Hello from server!");
+            },
+            onClose: () => {
+                console.log("Connection closed");
+            },
+        };
+    })
+);
 
 Deno.serve({ port: 4000 }, app.fetch);
