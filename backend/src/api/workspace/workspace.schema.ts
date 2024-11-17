a587e6e867552c377445ec085b644ed23f003220
AkashSDas <aakashdas368@gmail.com>
2024-11-17 15:22:54 +0530

    [be] add workspace create and delete endpoints schemas

--- backend/src/api/workspace/workspace.schema.ts
+++ backend/src/api/workspace/workspace.schema.ts
@@ -14,6 +14,35 @@ const InitializeWorkspaceResponseBody = z.object({
         .openapi({ example: "Account now has workspace feature available" }),
 });
 
+// ===============================================
+// Create New Workspace
+// ===============================================
+
+const CreateWorkspaceRequestBody = z.object({
+    /** Keeping these as "enum" due to low number of available workspaces */
+    workspaceImage: z.enum([
+        "workspace:go1.23",
+        "workspace:python3.13",
+        "workspace:vite-react18",
+        "workspace:hono4.6-deno2.0",
+    ]),
+});
+
+const CreateWorkspaceResponseBody = z.object({
+    workspaceURL: z.string().url().openapi({ example: "Workspace URL" }),
+});
+
+// ===============================================
+// Delete Workspace
+// ===============================================
+
+const DeleteWorkspaceParams = z.object({
+    workspaceId: z
+        .string()
+        .uuid()
+        .openapi({ example: "123e4567-e89b-12d3-a456-426655440000" }),
+});
+
 // ===================================
 // Exports
 // ===================================
@@ -22,4 +51,9 @@ const InitializeWorkspaceResponseBody = z.object({
 export const WorkspaceSchemas = {
     InitializeWorkspaceRequestBody,
     InitializeWorkspaceResponseBody,
+
+    CreateWorkspaceRequestBody,
+    CreateWorkspaceResponseBody,
+
+    DeleteWorkspaceParams,
 };
