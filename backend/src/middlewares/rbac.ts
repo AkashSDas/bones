a0b41d01fd2e3d2f3bac9b1751d7d4b8e3defdd5
AkashSDas <aakashdas368@gmail.com>
2024-11-24 19:58:49 +0530

    [be] add iam service wide rbac middleware

--- backend/src/middlewares/rbac.ts
+++ backend/src/middlewares/rbac.ts
@@ -26,12 +26,36 @@ const iamServiceWideWrite = createMiddleware<AppBindings>(async function (c, nex
 });
 
 // ============================
+// Workspace
+// ============================
+
+const workspaceServiceWideRead = createMiddleware<AppBindings>(
+    async function (c, next) {
+        const rbac = new RBACValidator(c);
+        rbac.validateWorkspaceServiceWide({ read: true, write: false });
+        return next();
+    },
+);
+
+const workspaceServiceWideWrite = createMiddleware<AppBindings>(
+    async function (c, next) {
+        const rbac = new RBACValidator(c);
+        rbac.validateWorkspaceServiceWide({ read: false, write: true });
+        return next();
+    },
+);
+
+// ============================
 // Export
 // ============================
 
 /** This middleware must be called after `authenticate` and `accountStatus` (if present) middleware. */
 export const rbac = {
     adminOnly,
+
     iamServiceWideRead,
     iamServiceWideWrite,
+
+    workspaceServiceWideRead,
+    workspaceServiceWideWrite,
 };
