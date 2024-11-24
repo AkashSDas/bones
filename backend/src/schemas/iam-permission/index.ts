fe5b77a2aca0020ca1ba5f7757eba35f59cf316b
AkashSDas <aakashdas368@gmail.com>
2024-11-24 09:09:56 +0530

    [be] remove import name alias

--- backend/src/schemas/iam-permission/index.ts
+++ backend/src/schemas/iam-permission/index.ts
@@ -1,15 +1,15 @@
 import { z } from "zod";
 
-import { IAMPolicies as _IAMPolicies } from "./iam";
+import { IAMPolicies } from "./iam";
 import { WorkspacePolicies } from "./workspace";
 
-export const IAMPolicies = {
-    IAM: _IAMPolicies,
+export const IAMPolicySchemas = {
+    IAM: IAMPolicies,
     Workspace: WorkspacePolicies,
 };
 
 export type IAMPolicy = {
-    IAMService: z.infer<typeof IAMPolicies.IAM.IAMService>;
+    IAMService: z.infer<typeof IAMPolicySchemas.IAM.IAMService>;
     Workspace: z.infer<typeof WorkspacePolicies.Workspace>;
     WorkspaceService: z.infer<typeof WorkspacePolicies.WorkspaceService>;
 };
