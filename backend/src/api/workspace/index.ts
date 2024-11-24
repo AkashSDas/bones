4f4224e717c1e39815a32b7bcb5018f9cddf2036
AkashSDas <aakashdas368@gmail.com>
2024-11-24 20:21:46 +0530

    [be] add endpoints for update and get workspace

--- backend/src/api/workspace/index.ts
+++ backend/src/api/workspace/index.ts
@@ -9,10 +9,9 @@ const router = app
     .openapi(routes.initializeWorkspace, handlers.initialize)
     .openapi(routes.deinitializeWorkspace, handlers.deinitialize)
     .openapi(routes.createWorkspace, handlers.createWorkspace)
-    .openapi(routes.deleteWorkspace, handlers.deleteWorkspace);
+    .openapi(routes.deleteWorkspace, handlers.deleteWorkspace)
+    .openapi(routes.getWorkspaces, handlers.getWorkspaces)
+    .openapi(routes.updateWorkspace, handlers.updateWorkspace)
+    .openapi(routes.getWorkspace, handlers.getWorkspace);
 
 export { router as workspaceRouter };
-
-// Endpoints:
-// 1. create a new workspace Kubernetes namespace (user will see a btn and click on
-// initialize namespace and this will save the namespace info in the DB -- new table)
--- backend/src/api/workspace/workspace.handlers.ts
+++ backend/src/api/workspace/workspace.handlers.ts
@@ -59,10 +59,70 @@ export const deleteWorkspace: WorkspaceHandler["DeleteWorkspace"] = async (c) =>
     }
 
     const rbac = new RBACValidator(c);
-    await rbac.validateWorkspace({ workspacePk: workspace[1], write: true });
+    rbac.validateWorkspace({ workspacePk: workspace[1], write: true });
 
     const manager = new WorkspaceManager(account.accountId);
     await manager.delete(workspace[1], workspace[2].workspaceId);
 
     return c.body(null, status.NO_CONTENT);
+};
+
+export const updateWorkspace: WorkspaceHandler["UpdateWorkspace"] = async (c) => {
+    const accountPk = c.get("accountPk")!;
+    const { workspaceId } = c.req.valid("param");
+    const body = c.req.valid("json");
+
+    const workspace = await dal.workspace.findByWorkspaceId(workspaceId);
+    if (!workspace) {
+        throw new NotFoundError({ message: "Workspace not found" });
+    }
+    if (workspace[0] !== accountPk) {
+        throw new ForbiddenError({
+            message: "You are not allowed to delete this workspace",
+        });
+    }
+
+    const rbac = new RBACValidator(c);
+    rbac.validateWorkspace({ workspacePk: workspace[1], write: true });
+
+    const updatedWorkspace = await dal.workspace.update(accountPk, workspace[1], body);
+
+    return c.json({ workspace: updatedWorkspace }, status.OK);
+};
+
+export const getWorkspace: WorkspaceHandler["GetWorkspace"] = async (c) => {
+    const accountPk = c.get("accountPk")!;
+    const { workspaceId } = c.req.valid("param");
+
+    const workspace = await dal.workspace.findByWorkspaceId(workspaceId);
+    if (!workspace) {
+        throw new NotFoundError({ message: "Workspace not found" });
+    }
+    if (workspace[0] !== accountPk) {
+        throw new ForbiddenError({
+            message: "You are not allowed to access this workspace",
+        });
+    }
+
+    const rbac = new RBACValidator(c);
+    rbac.validateWorkspace({ workspacePk: workspace[1], read: true });
+
+    return c.json({ workspace: workspace[2] }, status.OK);
+};
+
+export const getWorkspaces: WorkspaceHandler["GetWorkspaces"] = async (c) => {
+    const accountPk = c.get("accountPk")!;
+    const { offset, limit, search } = c.req.valid("query");
+
+    const rbac = new RBACValidator(c);
+    rbac.validateWorkspace({ workspacePk: accountPk, read: true });
+
+    const { totalCount, workspaces } = await dal.workspace.find(
+        accountPk,
+        search,
+        limit,
+        offset,
+    );
+
+    return c.json({ total: totalCount, workspaces }, status.OK);
 };
