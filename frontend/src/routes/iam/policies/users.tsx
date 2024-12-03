c492ff8ead382c587aa095a246da9407b8ffd6b8
AkashSDas <aakashdas368@gmail.com>
2024-12-03 19:13:27 +0530

    [fe] remove policy users page

--- frontend/src/routes/iam/policies/users.tsx
+++
@@ -1,5 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/iam/policies/users")({
-    component: () => <div>Hello /iam/policies/users!</div>,
-});
