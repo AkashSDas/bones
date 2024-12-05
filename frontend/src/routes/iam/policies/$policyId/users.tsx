3970a3338b6dcbc47a001b4986b668bbd98ef174
AkashSDas <aakashdas368@gmail.com>
2024-12-05 08:44:17 +0530

    [fe] add components in policy users page

--- frontend/src/routes/iam/policies/$policyId/users.tsx
+++ frontend/src/routes/iam/policies/$policyId/users.tsx
@@ -1,7 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { IAMUsersDialog } from "@/components/iam-permission/IAMUsersDialog";
 import { PolicyUsers } from "@/components/iam-permission/PolicyUsers";
 import { AuthProtected } from "@/components/shared/AuthProtected";
+import { Dialog } from "@/components/shared/Dialog";
+import { useGetApiV1IamPermissionPermissionId } from "@/gen/endpoints/iam-permission/iam-permission";
+import { useAuth } from "@/hooks/auth";
+import { iamKeys } from "@/utils/react-query";
 
 export const Route = createFileRoute("/iam/policies/$policyId/users")({
     component: () => (
@@ -14,5 +19,28 @@ export const Route = createFileRoute("/iam/policies/$policyId/users")({
 function IAMPolicyUsersView() {
     const { policyId } = Route.useParams();
 
-    return <PolicyUsers policyId={policyId} />;
+    const { authHeader } = useAuth();
+
+    const query = useGetApiV1IamPermissionPermissionId(policyId, {
+        axios: { headers: authHeader },
+        query: { queryKey: iamKeys.iamPolicy(policyId) },
+    });
+
+    const data = query.data?.data;
+    const users = data?.permission.users ?? [];
+
+    return (
+        <Dialog>
+            <PolicyUsers policyId={policyId} />
+            <IAMUsersDialog
+                permissionId={policyId}
+                excludeUsers={users.map((u) => {
+                    return {
+                        accessType: u.accessType,
+                        userId: u.userId,
+                    };
+                })}
+            />
+        </Dialog>
+    );
 }
