a54b5e9a5a9f9e7b1312c75a8e00c36cde39ea6f
AkashSDas <aakashdas368@gmail.com>
2024-12-06 19:32:53 +0530

    [fe] update react query key builder keys

--- frontend/src/utils/react-query.ts
+++ frontend/src/utils/react-query.ts
@@ -4,31 +4,42 @@
 export const iamKeys = {
     /** Logged in user details */
     me() {
-        return ["loggedInUser"] as const;
+        return ["iam", "loggedInUser"] as const;
     },
 
     /** Refresh's access token */
     refreshAccessToken() {
-        return ["refreshAccessToken"] as const;
+        return ["iam", "refreshAccessToken"] as const;
     },
 
     /** List of IAM users in an account */
     iamUsers(limit: string, offset: string, search: string | undefined) {
-        return ["iamUsers", limit, offset, search] as const;
+        return ["iam", "iamUsers", limit, offset, search] as const;
     },
 
     /** IAM user */
     iamUser(userId: string) {
-        return ["iamUser", userId];
+        return ["iam", "iamUser", userId];
     },
 
     /** List of IAM policies in an account */
     iamPolicies(limit: string, offset: string, search: string | undefined) {
-        return ["iamPolicies", limit, offset, search] as const;
+        return ["iam", "iamPolicies", limit, offset, search] as const;
     },
 
     /** IAM policy */
     iamPolicy(permissionId: string) {
-        return ["iamPolicy", permissionId];
+        return ["iam", "iamPolicy", permissionId];
+    },
+};
+
+/** Workspace related react query keys */
+export const workspaceKeys = {
+    checkInitialization() {
+        return ["workspace", "checkInitialization"] as const;
+    },
+
+    workspaces(limit: string, offset: string, search: string | undefined) {
+        return ["workspace", limit, offset, search] as const;
     },
 };
