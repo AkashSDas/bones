e98c4b98502a7526b61f50f691b415d5ccc647fa
AkashSDas <aakashdas368@gmail.com>
2024-12-09 23:46:17 +0530

    [fe] add state for managing workspace context window

--- frontend/src/store/workspace.ts
+++ frontend/src/store/workspace.ts
@@ -3,9 +3,14 @@ import { devtools } from "zustand/middleware";
 
 import { type GetApiV1WorkspaceWorkspaceId200Workspace } from "@/gen/schemas";
 
+type ContextWindow = "files" | "textSearch" | "fileSearch" | "networking" | null;
+
 type WorkspaceState = {
     workspace: GetApiV1WorkspaceWorkspaceId200Workspace | null;
     setWorkspace: (v: GetApiV1WorkspaceWorkspaceId200Workspace) => void;
+
+    contextWindow: ContextWindow;
+    setContextWindow: (v: ContextWindow) => void;
 };
 
 export const useWorkspaceStore = create<WorkspaceState>()(
@@ -14,6 +19,17 @@ export const useWorkspaceStore = create<WorkspaceState>()(
             workspace: null,
             setWorkspace(v) {
                 set((_state) => ({ workspace: v }));
+            },
+
+            contextWindow: "files",
+            setContextWindow(v) {
+                set((state) => {
+                    if (state.contextWindow === v) {
+                        return { contextWindow: null };
+                    } else {
+                        return { contextWindow: v };
+                    }
+                });
             },
         };
     }),
