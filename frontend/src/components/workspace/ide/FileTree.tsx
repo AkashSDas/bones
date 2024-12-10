a8663c9f93db8c561891c323841f7873ad920ee9
AkashSDas <aakashdas368@gmail.com>
2024-12-10 19:04:52 +0530

    [fe] add file tree handler in FileTree component

--- frontend/src/components/workspace/ide/FileTree.tsx
+++ frontend/src/components/workspace/ide/FileTree.tsx
@@ -1,97 +1,275 @@
-import { useMemo } from "react";
 import {
-    StaticTreeDataProvider,
-    Tree,
-    TreeItem,
-    TreeItemIndex,
-    UncontrolledTreeEnvironment,
-} from "react-complex-tree";
+    FilePlusIcon,
+    FolderPlusIcon,
+    ListCollapseIcon,
+    RotateCcwIcon,
+    TrashIcon,
+} from "lucide-react";
+import { ControlledTreeEnvironment, Tree } from "react-complex-tree";
 import "react-complex-tree/lib/style-modern.css";
 
-import { useWorkspaceFileTreeStore } from "@/store/workspace-file-tree";
+import { Button } from "@/components/shared/Button";
+import { Dialog, DialogTrigger } from "@/components/shared/Dialog";
+import { Loader } from "@/components/shared/Loader";
+import { useWorkspaceFileTree } from "@/hooks/workspace";
+import { useWorkspaceStore } from "@/store/workspace";
+import { findParentFile, useWorkspaceFileTreeStore } from "@/store/workspace-file-tree";
+import { cn } from "@/utils/styles";
 import { type File } from "@/utils/workspace-file-tree";
 
+import { AddFileOrFolderDialog } from "./AddFileOrFolderDialog";
 import { FileIcon } from "./FileIcon";
 
-function flatFileTree(workspaceFileTree: File): Record<TreeItemIndex, TreeItem<File>> {
-    const tree: Record<TreeItemIndex, TreeItem<File>> = {};
-
-    const flattenTree = (node: File): void => {
-        const { absolutePath, isDirectory, children } = node;
-
-        const sortedChildren = children
-            ? children.sort((a, b) => {
-                  if (a.isDirectory && !b.isDirectory) return -1; // Folders first
-                  if (!a.isDirectory && b.isDirectory) return 1; // Files after
-                  return a.name.localeCompare(b.name); // Alphabetical order
-              })
-            : [];
-
-        tree[absolutePath] = {
-            index: node.absolutePath,
-            data: node,
-            children: isDirectory
-                ? sortedChildren.map((child) => child.absolutePath)
-                : [],
-            isFolder: node.isDirectory,
-            canMove: true,
-            canRename: true,
-        };
-
-        if (children && Array.isArray(children)) {
-            children.forEach((child) => flattenTree(child));
-        }
-    };
-
-    flattenTree(workspaceFileTree);
-
-    return tree;
-}
+const TREE_ID = "file-tree";
 
 export function FileTree() {
-    const { fileTree } = useWorkspaceFileTreeStore();
-    const flatTree = useMemo(() => {
-        if (!fileTree) {
-            return {};
-        } else return flatFileTree(fileTree);
-    }, [fileTree]);
-
-    console.log({ fileTree });
+    const {
+        flatFileTree,
+        fileTree,
+        isFetching,
+        focusedFileTreeItems,
+        expandedFileTreeItems,
+        selectedFileTreeItems,
+        setCollapsedFileTreeItems,
+        setExpandedFileTreeItems,
+        setFocusedFileTreeItems,
+        setSelectedFileTreeItems,
+        setAddFileOrFolderInDirectory,
+        addFileOrFolderInDirectory,
+        setCollapsedFileTreeAllItems,
+        isDeletingFilesOrFolders,
+    } = useWorkspaceFileTreeStore();
+    const { getFileTree, deleteFilesOrFolders } = useWorkspaceFileTree();
+    const { contextWindow } = useWorkspaceStore();
 
     if (!fileTree) return null;
 
     return (
-        <UncontrolledTreeEnvironment<File>
-            dataProvider={new StaticTreeDataProvider(flatTree)}
-            getItemTitle={(item) => item.data.name}
-            viewState={{}}
-            canReorderItems={false}
-            canDrag={() => false}
-            canDragAndDrop
-            canDropOnFolder
-            renderItemTitle={({ title, item, context }) => {
-                return (
-                    <span className="flex items-center gap-2">
-                        <FileIcon
-                            filename={item.data.name}
-                            isDirectory={item.data.isDirectory}
-                            isFile={!item.data.isDirectory}
-                            isOpen={context.isExpanded}
-                            height={18}
-                            width={18}
-                        />
-                        <span className="overflow-hidden whitespace-nowrap text-nowrap text-ellipsis">
-                            {title}
-                        </span>
-                    </span>
-                );
-            }}
+        <div
+            className={cn(
+                "no-scrollbar h-full overflow-y-auto overflow-x-hidden border-r border-r-grey-900 !pb-12 group",
+                contextWindow === "files" ? "block" : "hidden",
+            )}
         >
-            <Tree
-                treeId="file-tree"
-                rootItem={fileTree.absolutePath} // Matches the ID assigned to the root in `flatFileTree`
-                treeLabel="Workspace File Tree"
-            />
-        </UncontrolledTreeEnvironment>
+            <div className="flex gap-[2px] mb-2 mt-2 px-2 items-center">
+                <span className="w-full text-sm tracking-wide uppercase text-grey-300">
+                    Workspace
+                </span>
+
+                <span className="opacity-0 group-hover:opacity-100 flex items-center gap-[2px]">
+                    <Button
+                        variant="ghost"
+                        size="icon"
+                        className="!h-7 !w-7"
+                        disabled={isFetching}
+                        onClick={getFileTree}
+                    >
+                        {isFetching ? (
+                            <Loader />
+                        ) : (
+                            <RotateCcwIcon className="stroke-grey-300" />
+                        )}
+                    </Button>
+
+                    <Button
+                        variant="ghost"
+                        size="icon"
+                        className="!h-7 !w-7"
+                        disabled={
+                            isFetching ||
+                            isDeletingFilesOrFolders ||
+                            selectedFileTreeItems.length === 0
+                        }
+                        onClick={() => {
+                            if (selectedFileTreeItems.length > 0) {
+                                deleteFilesOrFolders(
+                                    selectedFileTreeItems.map(
+                                        (item) => flatFileTree[item].data.absolutePath,
+                                    ),
+                                );
+                            }
+                        }}
+                    >
+                        {isDeletingFilesOrFolders ? (
+                            <Loader />
+                        ) : (
+                            <TrashIcon className="stroke-grey-300" />
+                        )}
+                    </Button>
+
+                    <Dialog
+                        open={addFileOrFolderInDirectory?.type === "file"}
+                        onOpenChange={(open) => {
+                            if (!open) {
+                                setAddFileOrFolderInDirectory(null);
+                            }
+                        }}
+                    >
+                        <AddFileOrFolderDialog />
+                        <DialogTrigger asChild>
+                            <Button
+                                variant="ghost"
+                                size="icon"
+                                className="!h-7 !w-7"
+                                disabled={isFetching}
+                                onClick={function addFile() {
+                                    // Get the selected files/folders
+
+                                    const selectedItems: File[] = [];
+                                    for (const selected of selectedFileTreeItems) {
+                                        selectedItems.push(flatFileTree[selected].data);
+                                    }
+
+                                    // Get the last selected folder (if exists)
+
+                                    let lastSelectedDirectory: null | string = null;
+                                    let lastSelectedFile: null | string = null;
+                                    for (const selected of selectedItems) {
+                                        if (selected.isDirectory) {
+                                            lastSelectedDirectory =
+                                                selected.absolutePath;
+                                        } else {
+                                            lastSelectedFile = selected.absolutePath;
+                                        }
+                                    }
+
+                                    // Get parent folder in which the folder will be added
+
+                                    let parentAbsolutePath = fileTree.absolutePath;
+                                    if (lastSelectedDirectory) {
+                                        parentAbsolutePath = lastSelectedDirectory;
+                                    } else if (lastSelectedFile) {
+                                        parentAbsolutePath =
+                                            findParentFile(fileTree, lastSelectedFile)
+                                                ?.absolutePath ?? fileTree.absolutePath;
+                                    }
+
+                                    setAddFileOrFolderInDirectory({
+                                        type: "file",
+                                        absolutePath: parentAbsolutePath,
+                                    });
+                                }}
+                            >
+                                <FilePlusIcon className="stroke-grey-300" />
+                            </Button>
+                        </DialogTrigger>
+                    </Dialog>
+
+                    <Dialog
+                        open={addFileOrFolderInDirectory?.type === "folder"}
+                        onOpenChange={(open) => {
+                            if (!open) {
+                                setAddFileOrFolderInDirectory(null);
+                            }
+                        }}
+                    >
+                        <AddFileOrFolderDialog />
+                        <DialogTrigger asChild>
+                            <Button
+                                variant="ghost"
+                                size="icon"
+                                className="!h-7 !w-7"
+                                disabled={isFetching}
+                                onClick={function addFolder() {
+                                    // Get the selected files/folders
+
+                                    const selectedItems: File[] = [];
+                                    for (const selected of selectedFileTreeItems) {
+                                        selectedItems.push(flatFileTree[selected].data);
+                                    }
+
+                                    // Get the last selected folder (if exists)
+
+                                    let lastSelectedDirectory: null | string = null;
+                                    let lastSelectedFile: null | string = null;
+                                    for (const selected of selectedItems) {
+                                        if (selected.isDirectory) {
+                                            lastSelectedDirectory =
+                                                selected.absolutePath;
+                                        } else {
+                                            lastSelectedFile = selected.absolutePath;
+                                        }
+                                    }
+
+                                    // Get parent folder in which the folder will be added
+
+                                    let parentAbsolutePath = fileTree.absolutePath;
+                                    if (lastSelectedDirectory) {
+                                        parentAbsolutePath = lastSelectedDirectory;
+                                    } else if (lastSelectedFile) {
+                                        parentAbsolutePath =
+                                            findParentFile(fileTree, lastSelectedFile)
+                                                ?.absolutePath ?? fileTree.absolutePath;
+                                    }
+
+                                    setAddFileOrFolderInDirectory({
+                                        type: "folder",
+                                        absolutePath: parentAbsolutePath,
+                                    });
+                                }}
+                            >
+                                <FolderPlusIcon className="stroke-grey-300" />
+                            </Button>
+                        </DialogTrigger>
+                    </Dialog>
+
+                    <Button
+                        variant="ghost"
+                        size="icon"
+                        className="!h-7 !w-7"
+                        disabled={isFetching}
+                        onClick={() =>
+                            setCollapsedFileTreeAllItems(expandedFileTreeItems)
+                        }
+                    >
+                        <ListCollapseIcon className="stroke-grey-300" />
+                    </Button>
+                </span>
+            </div>
+
+            <ControlledTreeEnvironment<File>
+                items={flatFileTree}
+                getItemTitle={(item) => item.data.name}
+                viewState={{
+                    [TREE_ID]: {
+                        focusedItem: focusedFileTreeItems.map((item) => item.index)[0],
+                        expandedItems: expandedFileTreeItems.map((item) => item.index),
+                        selectedItems: selectedFileTreeItems,
+                    },
+                }}
+                canReorderItems={false}
+                canDragAndDrop
+                onDrop={() => {}}
+                canRename
+                canDropOnFolder
+                onFocusItem={setFocusedFileTreeItems}
+                onExpandItem={setExpandedFileTreeItems}
+                onCollapseItem={setCollapsedFileTreeItems}
+                onSelectItems={setSelectedFileTreeItems}
+                renderItemTitle={({ title, item, context }) => {
+                    return (
+                        <span className="flex items-center gap-2">
+                            <FileIcon
+                                filename={item.data.name}
+                                isDirectory={item.data.isDirectory}
+                                isFile={!item.data.isDirectory}
+                                isOpen={context.isExpanded}
+                                height={20}
+                                width={20}
+                            />
+                            <span className="overflow-hidden whitespace-nowrap text-nowrap text-ellipsis">
+                                {title}
+                            </span>
+                        </span>
+                    );
+                }}
+            >
+                <Tree
+                    treeId={TREE_ID}
+                    rootItem={fileTree.absolutePath}
+                    treeLabel="Workspace File Tree"
+                />
+            </ControlledTreeEnvironment>
+        </div>
     );
 }
