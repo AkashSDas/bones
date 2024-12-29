4aba932fc90a20a7333023b3fc432cbd1f954438
AkashSDas <aakashdas368@gmail.com>
2024-12-29 12:01:40 +0530

    [fe] integrate lsp in code editor

--- frontend/src/components/workspace/ide/IdeEditor.tsx
+++ frontend/src/components/workspace/ide/IdeEditor.tsx
@@ -1,11 +1,34 @@
-import Editor, { useMonaco } from "@monaco-editor/react";
+import * as monaco from "monaco-editor";
+import getConfigurationServiceOverride from "@codingame/monaco-vscode-configuration-service-override";
+import getLanguagesServiceOverride from "@codingame/monaco-vscode-languages-service-override";
+import "@codingame/monaco-vscode-theme-defaults-default-extension";
+import Editor, { loader } from "@monaco-editor/react";
 import { shikiToMonaco } from "@shikijs/monaco";
-import { useEffect, useMemo } from "react";
+import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
+import { useMemo } from "react";
 import { createHighlighter } from "shiki";
+// We need to import this so monaco-languageclient can use vscode-api
+import "vscode/localExtensionHost";
+import { initialize } from "vscode/services";
 
 import { useWorkspaceStore } from "@/store/workspace";
 import { getEditorLanguage } from "@/utils/workspace-editor";
 import { type File } from "@/utils/workspace-file-tree";
+
+export type WorkerLoader = () => Worker;
+
+self.MonacoEnvironment = {
+    getWorker: (_moduleId, _label) => new editorWorker(),
+};
+
+loader.config({ monaco: monaco });
+
+await initialize({
+    ...getLanguagesServiceOverride(),
+    ...getConfigurationServiceOverride(),
+}).then(() => {
+    loader.init().then(/* ... */);
+});
 
 export function IdeEditor({ file, paneId }: { file: File; paneId: string }) {
     const { loadingFiles, files, setActivePaneId } = useWorkspaceStore();
@@ -17,23 +40,17 @@ export function IdeEditor({ file, paneId }: { file: File; paneId: string }) {
         [file],
     );
 
-    const monaco = useMonaco();
-
-    useEffect(() => {
-        if (monaco) {
-            (async function setupEditor() {})();
-        }
-    }, [monaco]);
-
     return (
         <Editor
             theme="vitesse-dark"
-            path={file.name}
+            path={file.absolutePath}
             defaultLanguage={language}
+            language={language}
             loading={loadingFiles.includes(file.absolutePath)}
             keepCurrentModel
             defaultValue={files[file.absolutePath] ?? ""}
             value={files[file.absolutePath] ?? ""}
+            defaultPath={file.absolutePath}
             beforeMount={async (monaco) => {
                 monaco.editor.onDidCreateEditor((editor) => {
                     editor.onDidFocusEditorText(function changeActivePane() {
@@ -43,15 +60,35 @@ export function IdeEditor({ file, paneId }: { file: File; paneId: string }) {
 
                 const highlighter = await createHighlighter({
                     themes: ["vitesse-dark", "one-dark-pro", "everforest-dark"],
-                    langs: ["javascript", "typescript", "jsx", "tsx", "toml", "yaml"],
+                    langs: [
+                        "javascript",
+                        "typescript",
+                        "jsx",
+                        "tsx",
+                        "toml",
+                        "yaml",
+                        "json",
+                        "python",
+                        "go",
+                        "html",
+                        "css",
+                        "markdown",
+                    ],
                 });
 
                 monaco.languages.register({ id: "jsx" });
-                monaco.languages.register({ id: "tsx" });
+                monaco.languages.register({ id: "tsx", extensions: ["tsx"] });
+                monaco.languages.register({ id: "html" });
+                monaco.languages.register({ id: "css" });
                 monaco.languages.register({ id: "toml" });
                 monaco.languages.register({ id: "yaml" });
-                monaco.languages.register({ id: "typescript" });
+                monaco.languages.register({ id: "json" });
+                monaco.languages.register({ id: "python" });
+                monaco.languages.register({ id: "go" });
+                monaco.languages.register({ id: "json", extensions: ["json"] });
+                monaco.languages.register({ id: "typescript", extensions: ["ts"] });
                 monaco.languages.register({ id: "javascript" });
+                monaco.languages.register({ id: "markdown" });
 
                 shikiToMonaco(highlighter, monaco);
             }}
@@ -70,6 +107,8 @@ export function IdeEditor({ file, paneId }: { file: File; paneId: string }) {
                     enabled: true,
                 },
                 fixedOverflowWidgets: true,
+                autoClosingBrackets: "always",
+                autoClosingQuotes: "always",
             }}
         />
     );
