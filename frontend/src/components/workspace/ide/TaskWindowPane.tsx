746171e539f47d32d078f5aea78652f6a487e355
AkashSDas <aakashdas368@gmail.com>
2025-01-06 23:21:12 +0530

    [fe] toggle task window pane based on z-index and visibility

--- frontend/src/components/workspace/ide/TaskWindowPane.tsx
+++ frontend/src/components/workspace/ide/TaskWindowPane.tsx
@@ -1,18 +1,33 @@
 import { useWorkspaceTaskWindowStore } from "@/store/workspace-task-window";
+import { cn } from "@/utils/styles";
 
 import { TerminalPane } from "./TerminalPane";
 
 export function TaskWindowPane() {
     const { activePaneId } = useWorkspaceTaskWindowStore();
 
-    switch (activePaneId) {
-        case "browserConsole": {
-            return <div>Browser Console</div>;
-        }
-        case "terminalSessions": {
-            return <TerminalPane />;
-        }
-        default:
-            return null;
-    }
+    return (
+        <div className="relative w-full h-full">
+            <div
+                className={cn(
+                    activePaneId === "browserConsole"
+                        ? "absolute w-full h-full opacity-100 visible"
+                        : "absolute w-full h-full opacity-0 invisible",
+                )}
+            >
+                <div>Browser Console</div>
+            </div>
+
+            <div
+                className={cn(
+                    activePaneId === "terminalSessions"
+                        ? "absolute w-full h-full opacity-100 visible"
+                        : "absolute w-full h-full opacity-0 invisible",
+                    "mb-3",
+                )}
+            >
+                <TerminalPane />
+            </div>
+        </div>
+    );
 }
