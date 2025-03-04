2dce9b80b81dc73e27508ea9ed4dd55082704c5e
AkashSDas <aakashdas368@gmail.com>
2025-03-04 23:52:05 +0530

    [be] change schema migration strategy and update Dockerfile

--- backend/src/db/migrate.ts
+++ backend/src/db/migrate.ts
@@ -6,14 +6,14 @@ import { connection, db } from "@/db";
 
 import config from "../../drizzle.config";
 
-if (!env.DB_MIGRATING) {
-    throw new Error(
-        "Environment variable for DB_MIGRATING is not set. Values can be 'true' or 'false'",
-    );
+if (env.DB_MIGRATING) {
+    // throw new Error(
+    //     "Environment variable for DB_MIGRATING is not set. Values can be 'true' or 'false'",
+    // );
+
+    (async () => {
+        await migrate(db, { migrationsFolder: config.out! });
+
+        await connection.end();
+    })();
 }
-
-(async () => {
-    await migrate(db, { migrationsFolder: config.out! });
-
-    await connection.end();
-})();
